3ef9627495024701d5e86035cff957e96131850a
Omkar1201 <omkarsalunkhe3597@gmail.com>
2023-08-02 20:44:28 +0530

    Updates

--- src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Navbar() {
-  return (
-    <div className='text-center font-extrabold text-3xl bg-slate-800 py-3 text-white'>Top Courses</div>
-  )
-}
